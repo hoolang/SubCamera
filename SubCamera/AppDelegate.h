982278dd449a178cefb1dd31897466ea439499fa
hoolang <hoolang@gmail.com>
2016-08-03 22:07:52 +0800

    Create

---
+++ SubCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SubCamera
+//
+//  Created by hoolang on 2016/8/3.
+//  Copyright © 2016年 Hoolang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SubCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SubCamera
+//
+//  Created by hoolang on 2016/8/3.
+//  Copyright © 2016年 Hoolang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
